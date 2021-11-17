82843e6c6b998e6a165dca4b41dc53f790ae75a0
Ties Westendorp <tieswestendorp@gmail.com>
2021-11-17 13:08:53 +0100

    Add toJSON method

---
+++ test/to_json.test.js
@@ -0,0 +1,24 @@
+const { ROBDD } = require('../lib/reduced-ordered-binary-decision-diagrams')
+
+describe('toJSON', () => {
+  test('returns the value as string for terminals', () => {
+    expect(ROBDD.True.toJSON()).toBe("true")
+    expect(ROBDD.False.toJSON()).toBe("false")
+  })
+
+  test('check the result of the operation on a BDD (1/2)', () => {
+    const x = ROBDD.variable()
+    const y = ROBDD.variable()
+    const bdd = ROBDD.or(x, y)
+
+    expect(bdd.toJSON()).toBe("{\"label\":0,\"then\":\"true\",\"else\":\"{\\\"label\\\":1,\\\"then\\\":\\\"true\\\",\\\"else\\\":\\\"false\\\"}\"}")
+  })
+
+  test('check the result of the operation on a BDD (2/2)', () => {
+    const x   = ROBDD.variable()
+    const y   = ROBDD.variable()
+    const bdd = ROBDD.and(x, y)
+
+    expect(bdd.toJSON()).toBe("{\"label\":0,\"then\":\"{\\\"label\\\":1,\\\"then\\\":\\\"true\\\",\\\"else\\\":\\\"false\\\"}\",\"else\":\"false\"}")
+  })
+})
